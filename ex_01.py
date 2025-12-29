89762fd3f9fc0a35f53cfdb3e22866f8543cbc1a
Ali Azhari <aliazhari@gmail.com>
2025-12-29 00:46:06 -0500

    First example of Pandas

---
+++ ex_01.py
@@ -0,0 +1,17 @@
+# * ******************************************
+ # * @Author : Ali Azhari   
+ # * Created On : Mon Dec 29 2025
+ # * @File : ex_01.py
+ # * 
+ #  * Description: First example using pandas
+# ******************************************/
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+print(df.to_string())
+
+
+
+
