a306f32424b801a13fba2a22c7d5fc53c80f8a80
Ali Azhari <aliazhari@gmail.com>
2025-12-29 00:56:34 -0500

    Find the version of Pandas

---
+++ ex_02.py
@@ -0,0 +1,14 @@
+# ******************************************
+ # * @Author : Ali Azhari   
+ # * Created On : Mon Dec 29 2025
+ # * @File : ex_02.py
+ # *
+ # * Description: Find the Pandas version
+#  * *****************************************
+
+import pandas as pd
+
+print(pd.__version__)
+
+
+
