f35190b74ee7621f885695a531b09d09542de7d8
Ali Azhari <aliazhari@gmail.com>
2025-12-29 01:14:49 -0500

    simple Pandas Series from a list

---
+++ ex_04.py
@@ -0,0 +1,16 @@
+# ******************************************
+ # * @Author : Ali Azhari   
+ # * Created On : Mon Dec 29 2025
+ # * @File : ex_04.py
+ # *
+ # * Description: 
+#  * *****************************************
+
+import pandas as pd
+
+a = [1, 7, 2]
+
+myvar = pd.Series(a)
+
+print(myvar)
+
